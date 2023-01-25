79d2a87e797ed5fb9877026b1c42b96b8e2ec073
Maria Akhaladze <makhaladze@kameleoon.com>
2023-01-26 01:23:30 +0400

    Moved models to models.py. Closes #7

--- routers/MetricsRouter.py
+++ routers/MetricsRouter.py
@@ -3,18 +3,9 @@
 
 from Connection import Connection
 from fastapi import APIRouter, status, Response
-from fastapi.responses import JSONResponse
-from pydantic import BaseModel
+from models import Metric, MetricSummary
 import mariadb
 
-class MetricSummary(BaseModel):
-    name: str
-    metric_value: float
-
-class Metric(BaseModel):
-    id: int
-    name: str
-    metric_value: float
 
 def connect():
     response = Response()
@@ -25,9 +16,11 @@ def connect():
             break
     return response
 
+
 router = APIRouter(prefix="/metrics",
-                            tags=["metrics"],
-                            responses={404: {"description": "Metrics router not found"}})
+                   tags=["metrics"],
+                   responses={404: {"description": "Metrics router not found"}})
+
 
 def compare_metrics(old_metric, new_metric):
     updates = []
@@ -35,6 +28,7 @@ def compare_metrics(old_metric, new_metric):
         if old_pair[1] != new_pair[1]:
             updates.append((new_pair[0], new_pair[1]))
     return updates
+
 
 def make_update_statement(metric_id, updates):
     statement = "update metric set "
@@ -45,7 +39,8 @@ def make_update_statement(metric_id, updates):
         inserts.append(pair[1])
     inserts.append(metric_id)
     statement += ", ".join(updates_row) + " where id = ?"
-    return statement, (*inserts, )
+    return statement, (*inserts,)
+
 
 def init_router():
     response = connect()
@@ -54,17 +49,18 @@ def init_router():
     else:
         connection = response.body
         cursor = response.body.cursor()
+
         @router.post("/", status_code=status.HTTP_201_CREATED)
         def add_metric(metric_body: MetricSummary, response: Response):
             try:
                 cursor.execute(
-                "insert into metric(name, metric_value) values (?, ?)", 
-                (metric_body.name, metric_body.metric_value))
+                    "insert into metric(name) values (?)",
+                    (metric_body.name, ))
                 connection.commit()
             except mariadb.Error as e:
                 response.status_code = status.HTTP_400_BAD_REQUEST
                 response.body = f"Could not create metric with body: {str(metric_body)}"
-                
+
         @router.delete("/{metric_id}", status_code=status.HTTP_200_OK)
         def delete_metric(metric_id: int, response: Response):
             try:
@@ -72,12 +68,12 @@ def init_router():
             except mariadb.Error as e:
                 response.status_code = status.HTTP_400_BAD_REQUEST
                 response.body = f"Could not delete metric with id = {str(metric_id)}"
-                
-        @router.put("/{metric_id}", status_code=status.HTTP_200_OK)        
+
+        @router.put("/{metric_id}", status_code=status.HTTP_200_OK)
         def update_metric(metric_id: int, metric_body: MetricSummary, response: Response):
             old_metric = get_metric(metric_id, response)
             if response.status_code == 200:
-                metric = Metric(id=metric_id, name=metric_body.name, metric_value=metric_body.metric_value)
+                metric = Metric(id=metric_id, name=metric_body.name)
                 updates = compare_metrics(old_metric, metric)
                 statement, inserts = make_update_statement(metric_id, updates)
                 if len(inserts) > 1:
@@ -96,24 +92,24 @@ def init_router():
                 result = cursor.fetchall()
                 if len(result) > 0:
                     for row in result:
-                        metrics.append(Metric(id=row[0], name=row[1], metric_value=row[2]))
+                        metrics.append(Metric(id=row[0], name=row[1]))
                 return metrics
             except mariadb.Error as e:
                 response.status_code = status.HTTP_400_BAD_REQUEST
                 response.body = "Could not get metrics, error = {e}"
-    
+
         @router.get("/{metric_id}", status_code=status.HTTP_200_OK)
         def get_metric(metric_id: int, response: Response):
             try:
-                cursor.execute("select * from metric where id = ?", (metric_id, ))
+                cursor.execute("select * from metric where id = ?", (metric_id,))
                 metric_raw = cursor.fetchall()
                 if len(metric_raw) == 0:
-                    raise mariadb.Error(f"Metric with id = {metric_id} not found") 
+                    raise mariadb.Error(f"Metric with id = {metric_id} not found")
             except mariadb.Error as e:
                 response.status_code = status.HTTP_404_NOT_FOUND
                 response.body = f"Metric with id = {metric_id} not found"
-            metric = Metric(id=metric_raw[0][0], name=metric_raw[0][1], 
-                        metric_value=metric_raw[0][2])
+            metric = Metric(id=metric_raw[0][0], name=metric_raw[0][1])
             return metric
-            
+
+
 init_router()
--- routers/UsersRouter.py
+++ routers/UsersRouter.py
@@ -3,12 +3,9 @@
 
 from Connection import Connection
 from fastapi import APIRouter, status, Response
-from fastapi.responses import JSONResponse
-from pydantic import BaseModel
 import mariadb
+from models import User
 
-class User(BaseModel):
-    id: int
 
 def connect():
     response = Response()
@@ -19,9 +16,11 @@ def connect():
             break
     return response
 
+
 router = APIRouter(prefix="/users",
-                            tags=["users"],
-                            responses={404: {"description": "Users router not found"}})
+                   tags=["users"],
+                   responses={404: {"description": "Users router not found"}})
+
 
 def compare_users(old_user, new_user):
     updates = []
@@ -29,6 +28,7 @@ def compare_users(old_user, new_user):
         if old_pair[1] != new_pair[1]:
             updates.append((new_pair[0], new_pair[1]))
     return updates
+
 
 def make_update_statement(user_id, updates):
     statement = "update users set "
@@ -39,7 +39,8 @@ def make_update_statement(user_id, updates):
         inserts.append(pair[1])
     inserts.append(user_id)
     statement += ", ".join(users_row) + " where id = ?"
-    return statement, (*inserts, )
+    return statement, (*inserts,)
+
 
 def init_router():
     response = connect()
@@ -48,15 +49,15 @@ def init_router():
     else:
         connection = response.body
         cursor = response.body.cursor()
+
         @router.post("/", status_code=status.HTTP_201_CREATED)
         def add_user(user_body: User, response: Response):
             try:
                 cursor.execute("insert into users(id) values (?)", (user_body.id,))
                 connection.commit()
-            except Exception  as e:
+            except Exception as e:
                 response.body = f"Could not create user with body: {str(user_body)}"
                 response.status_code = status.HTTP_400_BAD_REQUEST
-
 
         @router.delete("/{user_id}", status_code=status.HTTP_200_OK)
         def delete_user(user_id: int, response: Response):
@@ -79,18 +80,18 @@ def init_router():
             except mariadb.Error as e:
                 response.status_code = status.HTTP_400_BAD_REQUEST
                 response.body = "Could not get users, error = {e}"
-    
+
         @router.get("/{user_id}", status_code=status.HTTP_200_OK)
         def get_user(user_id: int, response: Response):
             try:
-                cursor.execute("select * from users where id = ?", (user_id, ))
+                cursor.execute("select * from users where id = ?", (user_id,))
                 user_raw = cursor.fetchall()
                 if len(user_raw) == 0:
                     raise mariadb.Error(f"User with id = {user_id} not found")
             except mariadb.Error as e:
                 response.status_code = status.HTTP_404_NOT_FOUND
                 response.body = f"User with id = {user_id} not found"
-            optimizer = User(id=user_raw[0][0],)
+            optimizer = User(id=user_raw[0][0], )
             return optimizer
 
         @router.put("/{user_id}", status_code=status.HTTP_200_OK)
@@ -106,4 +107,6 @@ def init_router():
                 except mariadb.Error as e:
                     response.status_code = status.HTTP_400_BAD_REQUEST
                     response.body = f"Could not update user with body: {str(user)}"
+
+
 init_router()
