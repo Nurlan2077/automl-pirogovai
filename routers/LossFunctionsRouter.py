d25be9bfe1bd3ecee4d56f0d7c65dfec3d8b73a9
Maria Akhaladze <makhaladze@kameleoon.com>
2023-01-26 17:13:41 +0400

    Rewritten responses for LossFunctionsRouter.py

--- routers/LossFunctionsRouter.py
+++ routers/LossFunctionsRouter.py
@@ -3,7 +3,9 @@
 
 from Connection import Connection
 from fastapi import APIRouter, status, Response
-from models import LossFunction, LossFunctionSummary
+from models import LossFunction, LossFunctionSummary, json_to_schema
+from fastapi.responses import JSONResponse
+from fastapi.encoders import jsonable_encoder
 import mariadb
 
 
@@ -51,24 +53,24 @@ def init_router():
         cursor = response.body.cursor()
 
         @router.post("/", status_code=status.HTTP_201_CREATED)
-        def add_loss_function(loss_function_body: LossFunctionSummary, response: Response):
+        def add_loss_function(loss_function_body: LossFunctionSummary):
             try:
                 cursor.execute("insert into loss_function(name) values (?)", (loss_function_body.name,))
                 connection.commit()
-            except Exception as e:
-                response.body = f"Could not create loss function with body: {str(loss_function_body)}"
-                response.status_code = status.HTTP_400_BAD_REQUEST
+            except mariadb.Error:
+                return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST,
+                                    content=f"Could not create loss function with body: {str(loss_function_body)}")
 
         @router.delete("/{loss_function_id}", status_code=status.HTTP_200_OK)
-        def delete_loss_function(loss_function_id: int, response: Response):
+        def delete_loss_function(loss_function_id: int):
             try:
                 cursor.execute("delete from loss_function where id = ?", (loss_function_id,))
-            except mariadb.Error as e:
-                response.status_code = status.HTTP_400_BAD_REQUEST
-                response.body = f"Could not delete loss function with id = {str(loss_function_id)}"
+            except mariadb.Error:
+                return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST,
+                                    content=f"Could not delete loss function with id = {str(loss_function_id)}")
 
         @router.get("/", status_code=status.HTTP_200_OK)
-        def get_loss_functions(response: Response):
+        def get_loss_functions():
             try:
                 cursor.execute("select * from loss_function")
                 loss_functions = []
@@ -76,37 +78,44 @@ def init_router():
                 if len(result) > 0:
                     for row in result:
                         loss_functions.append(LossFunction(id=row[0], name=row[1]))
-                return loss_functions
-            except mariadb.Error as e:
-                response.status_code = status.HTTP_400_BAD_REQUEST
-                response.body = "Could not get loss functions, error = {e}"
+                return JSONResponse(status_code=status.HTTP_200_OK,
+                                    content=jsonable_encoder(loss_functions))
+            except mariadb.Error:
+                return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST,
+                                    content="Could not get loss functions")
 
         @router.get("/{loss_function_id}", status_code=status.HTTP_200_OK)
-        def get_loss_function(loss_function_id: int, response: Response):
+        def get_loss_function(loss_function_id: int):
             try:
                 cursor.execute("select * from loss_function where id = ?", (loss_function_id,))
                 function_raw = cursor.fetchall()
                 if len(function_raw) == 0:
-                    raise mariadb.Error(f"Loss function with id = {loss_function_id} not found")
-            except mariadb.Error as e:
-                response.status_code = status.HTTP_404_NOT_FOUND
-                response.body = f"Loss function with id = {loss_function_id} not found"
-            loss_function = LossFunction(id=function_raw[0][0], name=function_raw[0][1])
-            return loss_function
+                    return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,
+                                        content=f"Loss function with id = {loss_function_id} not found")
+                loss_function = LossFunction(id=function_raw[0][0], name=function_raw[0][1])
+                return JSONResponse(status_code=status.HTTP_200_OK,
+                                    content=jsonable_encoder(loss_function))
+            except mariadb.Error:
+                return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST,
+                                    content=f"Could not get loss function with id = {loss_function_id}")
 
         @router.put("/{loss_function_id}", status_code=status.HTTP_200_OK)
-        def update_loss_function(loss_function_id: int, loss_function: LossFunctionSummary, response: Response):
-            old_loss_function = get_loss_function(loss_function_id, response)
-            loss_function = LossFunction(id=loss_function_id, name=loss_function.name)
-            updates = compare_functions(old_loss_function, loss_function)
-            statement, inserts = make_update_statement(loss_function_id, updates)
-            if len(inserts) > 1:
-                try:
-                    cursor.execute(statement, inserts)
-                    connection.commit()
-                except mariadb.Error as e:
-                    response.status_code = status.HTTP_400_BAD_REQUEST
-                    response.body = f"Could not update loss function with body: {str(loss_function)}"
+        def update_loss_function(loss_function_id: int, loss_function: LossFunctionSummary):
+            get_response = get_loss_function(loss_function_id)
+            if get_response.status_code == status.HTTP_200_OK:
+                old_loss_function = json_to_schema(get_response.body, LossFunction)
+                loss_function = LossFunction(id=loss_function_id, name=loss_function.name)
+                updates = compare_functions(old_loss_function, loss_function)
+                statement, inserts = make_update_statement(loss_function_id, updates)
+                if len(inserts) > 1:
+                    try:
+                        cursor.execute(statement, inserts)
+                        connection.commit()
+                    except mariadb.Error:
+                        return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST,
+                                            content=f"Could not update loss function with body: {str(loss_function)}")
+            else:
+                return get_response
 
 
 init_router()
