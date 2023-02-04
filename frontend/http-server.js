5701dfd8752c35dda0f3cf78386c2df23361ff51
Maria Akhaladze <makhaladze@kameleoon.com>
2023-02-05 03:18:46 +0400

    Added frontend server

--- frontend/http-server.js
+++ frontend/http-server.js
@@ -1,31 +1,30 @@
 const http = require('http');
 const fs = require('fs');
-const path = require("path");
 
 const hostname = '0.0.0.0';
 const port = 3000;
 
 const server = http.createServer((req, response) => {
     console.log(`Serving request for ${req.url}`);
-    const extname = path.extname(req.url);
-    if (extname !== '.js' && extname !== '.css') {
-        let filePath = `./html${req.url === '/' ? '/index.html' : req.url}`;
+    let filePath;
+    if (req.url.endsWith(".css") || req.url.endsWith(".js") || req.url.endsWith(".ico")) {
+        filePath = `.${req.url}`
+    }
+    else {
+        filePath = `./html${req.url === '/' ? '/index.html' : req.url}`;
         if (!filePath.endsWith(".html"))
             filePath += ".html";
-        fs.readFile(filePath, (error, data) => {
-            if (error) {
-                console.error(`Error reading file: ${error}`);
-                response.writeHead(404, {'Content-Type': 'text/plain'});
-                response.end('File not found');
-            } else {
-                response.writeHead(200, {'Content-Type': 'text/html'});
-                response.end(data);
-            }
-        });
-    } else {
-        response.writeHead(200, {'Content-Type': 'text/html'});
-        response.end();
     }
+    fs.readFile(filePath, (error, data) => {
+        if (error) {
+            console.error(`Error reading file: ${error}`);
+            response.writeHead(404, {'Content-Type': 'text/plain'});
+            response.end('File not found');
+        } else {
+            response.writeHead(200, {'Content-Type': 'text/html'});
+            response.end(data);
+        }
+    });
 });
 
 server.listen(port, hostname, (error) => {
