7519048be07f50c1a0808a585708940c4b73e78e
Maria Akhaladze <makhaladze@kameleoon.com>
2023-02-05 21:47:01 +0400

    Added authorization method and new user fields, Closes #4

--- backend/routers/UsersRouter.py
+++ backend/routers/UsersRouter.py
@@ -7,7 +7,7 @@ from fastapi.responses import JSONResponse
 from fastapi.encoders import jsonable_encoder
 import mariadb
 from .models import User, json_to_schema
-from .utils import compare_items, make_update_statement, get_created_id
+from .utils import compare_items, make_update_statement
 import logging
 
 logging.basicConfig(level=logging.INFO,
@@ -21,18 +21,37 @@ router = APIRouter(prefix="/users",
                    responses={404: {"description": "Users router not found"}})
 
 
+@router.post("/authorize")
+def authorize(user: User):
+    get_response = get_user(user.id)
+    if get_response.status_code == status.HTTP_200_OK:
+        update_response = update_user(user.id, user)
+        if update_response.status_code == status.HTTP_200_OK:
+            return JSONResponse(status_code=status.HTTP_200_OK,
+                                content={"id": user.id})
+        else:
+            return update_response
+    else:
+        create_response = add_user(user)
+        if create_response.status_code == status.HTTP_201_CREATED:
+            return JSONResponse(status_code=status.HTTP_200_OK,
+                                content={"id": user.id})
+        else:
+            return create_response
+
+
 @router.post("/", status_code=status.HTTP_201_CREATED)
-def add_user(user_body: User):
+def add_user(user: User):
     try:
-        cursor.execute("insert into users(id) values (?)", (user_body.id,))
+        cursor.execute("insert into users(id, name, email) values (?, ?, ?)", (user.id, user.name, user.email))
         connection.commit()
-        entity_id = get_created_id(cursor, "users")[0][0]
-        logging.info(f"User with body = {str(user_body)} has been created successfully")
-        return {"id": entity_id}
+        logging.info(f"User with body = {str(user)} has been created successfully")
+        return JSONResponse(status_code=status.HTTP_201_CREATED,
+                            content={"id": user.id})
     except mariadb.Error as e:
-        logging.error(f"Could not create user with body: {str(user_body)}. Error: {e}")
+        logging.error(f"Could not create user with body: {str(user)}. Error: {e}")
         return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST,
-                            content=f"Could not create user with body: {str(user_body)}")
+                            content=f"Could not create user with body: {str(user)}")
 
 
 @router.delete("/{user_id}", status_code=status.HTTP_200_OK)
@@ -54,7 +73,7 @@ def get_users():
         result = cursor.fetchall()
         if len(result) > 0:
             for row in result:
-                users.append(User(id=row[0]))
+                users.append(User(id=row[0], name=row[1], email=row[2]))
         return JSONResponse(status_code=status.HTTP_200_OK,
                             content=jsonable_encoder(users))
     except mariadb.Error as e:
@@ -72,7 +91,7 @@ def get_user(user_id: int):
             logging.warning(f"User with id = {user_id} not found")
             return JSONResponse(status_code=status.HTTP_404_NOT_FOUND,
                                 content=f"User with id = {user_id} not found")
-        user = User(id=user_raw[0][0], )
+        user = User(id=user_raw[0][0], name=user_raw[0][1], email=user_raw[0][2])
         return JSONResponse(status_code=status.HTTP_200_OK,
                             content=jsonable_encoder(user))
     except mariadb.Error as e:
@@ -81,12 +100,12 @@ def get_user(user_id: int):
                             content=f"Could not get user with id = {user_id}")
 
 
-@router.put("/{user_id}", status_code=status.HTTP_200_OK)
+@router.put("/{user_id}")
 def update_user(user_id: int, user: User):
     get_response = get_user(user_id)
     if get_response.status_code == status.HTTP_200_OK:
         old_user = json_to_schema(get_response.body, User)
-        user = User(id=user.id)
+        user = User(id=user.id, name=user.name, email=user.email)
         updates = compare_items(old_user, user)
         statement, inserts = make_update_statement([user_id], "users", ["id"], updates)
         if len(inserts) > 1:
@@ -94,9 +113,14 @@ def update_user(user_id: int, user: User):
                 cursor.execute(statement, inserts)
                 connection.commit()
                 logging.info(f"User with id = {user_id} has been updated successfully")
+                return JSONResponse(status_code=status.HTTP_200_OK,
+                                    content=f"User with id = {user_id} has been updated successfully")
             except mariadb.Error as e:
                 logging.error(f"Could not update user with body: {str(user)}. Error: {e}")
                 return JSONResponse(status_code=status.HTTP_400_BAD_REQUEST,
                                     content=f"Could not update user with body: {str(user)}")
+        else:
+            return JSONResponse(status_code=status.HTTP_200_OK,
+                                content=f"User with id = {user_id} already has body: {str(user)}")
     else:
         return get_response
