3ea0932c05a751925fa8e8d18eafab9b716cfa9e
Aleksandr Mochalov <mochalovalexalex@outlook.com>
2023-02-06 21:34:36 +0500

    Добавление ml

--- loss_funcs.py
+++
@@ -1,8 +0,0 @@
-from keras import losses
-
-LOSS_FUNCS = [
-    losses.SparseCategoricalCrossentropy(from_logits=True),
-    losses.CategoricalHinge(),
-    losses.KLDivergence(),
-    losses.Poisson()
-]
