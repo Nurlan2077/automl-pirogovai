2c0cdcb3036a048a92e87447833b99603060eaab
MSenso <39305933+MSenso@users.noreply.github.com>
2023-03-18 01:09:37 +0400

    37 add logout. Close #37 (#52)
    * 31 login page. Close #31 (#49)
    
    * login implementation
    
    * Fixed async awaiting and userId passing to session
    
    * Added temporary endpoint on backend for login
    
    ---------
    
    Co-authored-by: ChDarina <dvchupina@edu.hse.ru>
    Co-authored-by: Maria Akhaladze <makhaladze@kameleoon.com>
    
    * added logout
    
    * Fixed typo in userId
    
    ---------
    
    Co-authored-by: ChDarina <dvchupina@edu.hse.ru>
    Co-authored-by: Maria Akhaladze <makhaladze@kameleoon.com>

---
+++ frontend/html/resources/js/redirect.js
@@ -0,0 +1,20 @@
+const userId = sessionStorage.getItem("userId");
+
+// If user_id is undefined, redirect to the login page
+if (!userId) {
+  window.location.replace("http://0.0.0.0:3000/index");
+  alert("Вы не вошли в аккаунт");
+}
+
+// Make the GET request
+const url = `http://pirogov-backend.net:8000/users/${userId}`;
+fetch(url)
+  .then(response => {
+    if (response.status !== 200) {
+      window.location.replace("http://0.0.0.0:3000/index");
+      alert("Вы не вошли в аккаунт");
+    }
+  })
+  .catch(error => {
+    console.error("Error:", error);
+  });
